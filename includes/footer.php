9e08da83c30745b8fa18a0b9bdd20d8098594f5e
venkatpavan12 <venkatpavan342@gmail.com>
2022-09-28 12:57:25 +0530

    Bank management system

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; BloodBank & Donor Management System </p>
+             <p class="m-1 text-center text-white"> JNTUK UCEV Vizianagaram 2022</p>
+        </div>
+        <!-- /.container -->
+    </footer>
